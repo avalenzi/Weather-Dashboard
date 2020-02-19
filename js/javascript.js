f97a2f2cc53f9017ae911446c65c17e483e6b673
Anthony Valenzi <anthonymv1313@gmail.com>
2020-02-18 22:44:07 -0700

    updated all folders push

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,4 @@
-// Weather Homework URLS: 
+//Urls
 // var currentWeather =  "http://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=7ba67ac190f85fdba2e2dc6b9d32e93c&units=imperial";
 // Forecast: "http://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=7ba67ac190f85fdba2e2dc6b9d32e93c&units=imperial"
 // Icon: "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png"
@@ -7,9 +7,7 @@ var $forecast = $(".forecast")
 
 
 // Weather functions
-// Forecast function
 function currentWeather() {
-
     var searchValue = $("input").val().trim()
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=30e922552c21f651e398ab194a1b1c83&units=imperial";
 
@@ -22,6 +20,8 @@ function currentWeather() {
         // waiting for the ajax call to resolve
         .then(function (data) {
 
+            $("#cityResults").empty();
+
             console.log(data);
             // Creates a div to hold the city 
             var $data = $("<div>")
@@ -32,9 +32,11 @@ function currentWeather() {
             // Displays the city name
             $data.append($cityName);
             // Retrieves the time Data
-            var time = moment().subtract(10, 'days').calendar();
+            var time = moment().format('L');
+            // image icon
+            var image = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
             // appending the time to the city name
-            $cityName.append(" " + time);
+            $cityName.append(" " + time + " " + image);
             // Displays the city name
             $data.append($cityName);
             // retrieve the temp
@@ -55,49 +57,57 @@ function currentWeather() {
         });
 }
 
-// Icon: 
-// function icon () {
-
-//     var searchValue = $("input").val().trim()
-//     var queryURL = "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png"
-
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     })
-
-//     // waiting for the ajax call to resolve
-//     .then(function (data) {
-//         console.log(data);
-//     })
-// };
-
-
 // On any button click
 function forecast() {
 
     var searchValue = $("input").val().trim()
     var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=30e922552c21f651e398ab194a1b1c83&units=imperial"
 
-    // requesting the data 
+    // requesting data 
     $.ajax({
         url: queryURL,
         method: "GET"
     })
 
-        // waiting for the ajax call to resolve
+     
         .then(function (data) {
-            console.log(data);
-        })
+            $("#cast").empty();
 
+            console.log(data);
+
+            for (var i = 0; i < 5; i++) {
+  
+                var $data = $("<div>").addClass("cast");
+                var $date = $("<h5>").text(moment(data.list[i].dt).format("L"));
+                console.log(moment(data.list[i].dt).format("L"));
+                var $temp = $("<p>").text("temp: " + data.list[i].main.temp + " °F");
+                console.log(data.list[i].main.temp);
+                var $humidity = $("<p>").text("humidity: " + data.list[i].main.humidity + " %");
+                console.log(data.list[i].main.humidity);
+                $data.append(
+                    $date,
+                    $temp,
+                    $humidity
+                )
+                $("#cast").append($data);
+            }
+        })
+        
 };
 
-$("button").on("click", function () {
+$("button").on("click", function click () {
 
     var searchValue = $("input").val().trim()
     var $cityLi = $("<button>").text(searchValue);
+    $cityLi.addClass("cityButton");
     $("#searchInput").append($cityLi);
-    
+
+    currentWeather();
+    forecast();
+});
+
+$(document).on("click", ".cityButton", function (){
+
     currentWeather();
     forecast();
 });
