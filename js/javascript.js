b8a207389aeb2f90a65db877532fec4888da55bf
Anthony Valenzi <anthonymv1313@gmail.com>
2020-02-24 18:43:42 -0700

    start of updated js

--- js/javascript.js
+++ js/javascript.js
@@ -3,111 +3,79 @@
 // Forecast: "http://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=7ba67ac190f85fdba2e2dc6b9d32e93c&units=imperial"
 // Icon: "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png"
 
-var $forecast = $(".forecast")
+$(document).ready(function () {
+    var apiKey = "30e922552c21f651e398ab194a1b1c83";
+    var $temp = $("#temp");
+    var $humidity = $("#humidity");
+    var $wind = $("#wind");
+    var $uv = $("#uv");
+    var $city = $("#city");
+    var $search = $("#search");
+    var $searchBtn = $("#searchBtn");
 
+    var searchArray = localStorage.getItem("localSearch");
+    if (searchArray) {
+        searchArray = JSON.parse(searchArray);
+    } else {
+        searchArray = [];
+    }
 
-// Weather functions
-function currentWeather() {
-    var searchValue = $("input").val().trim()
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=30e922552c21f651e398ab194a1b1c83&units=imperial";
+    getSearch();
 
-    // requesting the data 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
+    // On click event to grab search input/hidden div
+    $searchBtn.on("click", function (event) {
+        event.preventDefault();
+        var search = $search.val();
+        $(".hide").removeClass("hide");
 
-        // waiting for the ajax call to resolve
-        .then(function (data) {
+        storeSearch(search);
+        getWeather(search);
+    });
 
-            $("#cityResults").empty();
+    // function built to create AJAX call from search form input
+    function getWeather(search) {
+        var queryURL =
+            "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" +
+            search +
+            "&appid=" +
+            apiKey;
+        // AJAX call to get current weather 
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            var dateString = moment.unix(response.dt).format("MM/DD/YYYY");
+            $city.text(response.name + " (" + dateString + ")");
+            $temp.text("Temperature: " + response.main.temp + " °F");
+            $humidity.text("Humidity: " + response.main.humidity + " %");
+            $wind.text("Wind Speed: " + response.wind.speed + " MPH");
+            weatherIconUrl =
+                "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+            weatherImage = $("<img>").attr("src", weatherIconUrl);
+            $city.append(weatherImage);
+            var latitude = response.coord.lat;
+            var longitude = response.coord.lon;
+            var uvIndexQueryUrl =
+                "https://api.openweathermap.org/data/2.5/uvi?&appid=" +
+                apiKey +
+                "&lat=" +
+                latitude +
+                "&lon=" +
+                longitude;
+            // AJAX call to get UV Index information
+            $.ajax({
+                url: uvIndexQueryUrl,
+                method: "GET"
+            }).then(function (returned) {
+                $uv.text("UV Index: " + returned.value);
 
-            console.log(data);
-            // Creates a div to hold the city 
-            var $data = $("<div>")
-            // Retrieves the City name Data
-            var cityName = data.name;
-            // Creates an element to have the city name displayed
-            var $cityName = $("<h2>").text(cityName);
-            // Displays the city name
-            $data.append($cityName);
-            // Retrieves the time Data
-            var time = moment().format('L');
-            // image icon
-            var image = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
-            // appending the time to the city name
-            $cityName.append(" " + time + " " + image);
-            // Displays the city name
-            $data.append($cityName);
-            // retrieve the temp
-            var temp = $("<p>").text("Temperature: " + data.main.temp + " °F");
-            // display the temp
-            $data.append(temp);
-            // retrieve the humidity
-            var humidity = $("<p>").text("Humidity: " + data.main.humidity + " %");
-            // display the humidity
-            $data.append(humidity);
-            // retrieve the wind speed
-            var windSpeed = $("<p>").text("Wind Speed: " + data.wind.speed + " MPH");
-            // display the temp
-            $data.append(windSpeed);
-
-            $("#cityResults").append($data);
-
-        });
-}
-
-// On any button click
-function forecast() {
-
-    var searchValue = $("input").val().trim()
-    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=30e922552c21f651e398ab194a1b1c83&units=imperial"
-
-    // requesting data 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-
-     
-        .then(function (data) {
-            $("#cast").empty();
-
-            console.log(data);
-
-            for (var i = 0; i < 5; i++) {
-  
-                var $data = $("<div>").addClass("cast");
-                var $date = $("<h5>").text(moment(data.list[i].dt).format("L"));
-                console.log(moment(data.list[i].dt).format("L"));
-                var $temp = $("<p>").text("temp: " + data.list[i].main.temp + " °F");
-                console.log(data.list[i].main.temp);
-                var $humidity = $("<p>").text("humidity: " + data.list[i].main.humidity + " %");
-                console.log(data.list[i].main.humidity);
-                $data.append(
-                    $date,
-                    $temp,
-                    $humidity
-                )
-                $("#cast").append($data);
-            }
-        })
-        
-};
-
-$("button").on("click", function click () {
-
-    var searchValue = $("input").val().trim()
-    var $cityLi = $("<button>").text(searchValue);
-    $cityLi.addClass("cityButton");
-    $("#searchInput").append($cityLi);
-
-    currentWeather();
-    forecast();
-});
-
-$(document).on("click", ".cityButton", function (){
-
-    currentWeather();
-    forecast();
-});
+                if (returned.value <= 2) {
+                    $uv.addClass("favorable");
+                }
+                if (returned.value > 2 && returned.value <= 7) {
+                    $uv.addClass("moderate");
+                }
+                if (returned.value > 7) {
+                    $uv.addClass("severe");
+                }
+            });
